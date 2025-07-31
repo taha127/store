e2de36a1facc90468b7e208ba0fd6dc4d40c8ed1
taha127 <tahadehghani127@gmail.com>
2025-07-31 19:50:55 +0330

    Add initial models and migration for store application

--- store/models.py
+++ store/models.py
@@ -1,3 +1,102 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=500, blank=True)
+    top_product = models.ForeignKey(
+        'Product', on_delete=models.SET_NULL, null=True, related_name='+')
+
+
+class Discount(models.Model):
+    discount = models.FloatField()
+    description = models.CharField(max_length=500)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    category = models.ForeignKey(
+        Category, on_delete=models.PROTECT, related_name='products')
+    slug = models.SlugField()
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.PositiveIntegerField()
+    detatime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now=True)
+    discounts = models.ManyToManyField(Discount, blank=True)
+
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=255)
+
+
+class Address(models.Model):
+    customer = models.ForeignKey(
+        Customer, on_delete=models.CASCADE, primary_key=True)
+    province = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    street = models.CharField(max_length=255)
+
+
+class Order(models.Model):
+    ORDER_STATUS_PAID = 'p'
+    ORDER_STATUS_UNPAID = 'u'
+    ORDER_STATUS_CANCELLED = 'c'
+    ORDER_STATUS_CHOICES = [
+        (ORDER_STATUS_PAID, 'Paid'),
+        (ORDER_STATUS_UNPAID, 'Unpaid'),
+        (ORDER_STATUS_CANCELLED, 'Cancelled'),
+    ]
+    customer = models.ForeignKey(
+        Customer, on_delete=models.PROTECT, related_name='orders')
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(
+        max_length=1, choices=ORDER_STATUS_CHOICES, default=ORDER_STATUS_UNPAID)
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(
+        Order, on_delete=models.PROTECT, related_name='items')
+    product = models.ForeignKey(
+        Product, on_delete=models.PROTECT, related_name='order_items')
+    quantity = models.PositiveSmallIntegerField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    class Meta:
+        unique_together = [['order', 'product']]
+
+
+class Cart(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(
+        Cart, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='cart_items')
+    quantity = models.PositiveSmallIntegerField()
+
+    class Meta:
+        unique_together = [['cart', 'product']]
+
+
+class Comment(models.Model):
+    COMMENT_STATUS_APPROVED = 'a'
+    COMMENT_STATUS_PENDING = 'p'
+    COMMENT_STATUS_REJECTED = 'r'
+    COMMENT_STATUS_CHOICES = [
+        (COMMENT_STATUS_APPROVED, 'Approved'),
+        (COMMENT_STATUS_PENDING, 'Pending'),
+        (COMMENT_STATUS_REJECTED, 'Rejected'),
+    ]
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name='comments')
+    # TODO: May be need to change to a ForeignKey to Customer
+    name = models.CharField(max_length=255)
+    body = models.TextField()
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(
+        max_length=1, choices=COMMENT_STATUS_CHOICES, default=COMMENT_STATUS_PENDING)
