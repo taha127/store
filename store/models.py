eca6a3f56eafe25b32d859499789bd264f0561bd
taha127 <tahadehghani127@gmail.com>
2025-08-10 14:12:11 +0330

    Add __str__ methods to models for better string representation

--- store/models.py
+++ store/models.py
@@ -5,12 +5,17 @@ class Category(models.Model):
     title = models.CharField(max_length=255)
     description = models.CharField(max_length=500, blank=True)
     top_product = models.ForeignKey(
-        'Product', on_delete=models.SET_NULL, null=True, related_name='+')
+
+    def __str__(self):
+        return self.title
 
 
 class Discount(models.Model):
     discount = models.FloatField()
     description = models.CharField(max_length=500)
+
+    def __str__(self):
+        return f'{str(self.discount)} | {self.description}'
 
 
 class Product(models.Model):
@@ -25,12 +30,18 @@ class Product(models.Model):
     datetime_modified = models.DateTimeField(auto_now=True)
     discounts = models.ManyToManyField(Discount, blank=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Customer(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.EmailField()
     phone_number = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
 
 
 class Address(models.Model):
@@ -94,7 +105,6 @@ class Comment(models.Model):
     ]
     product = models.ForeignKey(
         Product, on_delete=models.CASCADE, related_name='comments')
-    # TODO: May be need to change to a ForeignKey to Customer
     name = models.CharField(max_length=255)
     body = models.TextField()
     datetime_created = models.DateTimeField(auto_now_add=True)
