df162aae19bd01de6ed7318c189470703e0db923
taha127 <tahadehghani127@gmail.com>
2025-08-10 14:25:26 +0330

    Customizes admin site title and header

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,10 @@ from django.urls import path, include
 from django.conf import settings
 import debug_toolbar
 
+
+admin.site.site_header = 'Store'
+admin.site.index_title = 'Special Access'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('store/', include('store.urls')),
